7d7e7b7cadc077a1db1487b1f1b409adf6dfc87f
DYMUJIJIO <85023291+DYMUJIJIO@users.noreply.github.com>
2021-06-19 19:17:56 +0300

    добавлено несколько функиц

--- SANDBOX/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ SANDBOX/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,20 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.org.apache.xpath.internal.objects.XString;
+
 public class MyFirstProgram {
-public static void main(String[] args){
-System.out.println("Hello, world!");
-}
+    public static void main(String[] args) {
+       hello("world");
+        hello("user");
+double l = 5;
+System.out.println ("площадь квадрата со стороной" + l + "=" + area(l));
+double a = 4;
+double b = 5;
+        System.out.println("площадь прям со сторонами" + a + "и" + b+"="+area(a, b));
+    }
+    public  static  void hello(String somebody){
+
+System.out.println("Hello," + somebody +"!");}
+    public static double area(double l){ return l*l;}
+    public static double area(double a, double b){ return a*b;}
 }
